8df7969de329c49ec7cb4074fea6c0382d8c5eea
nha <nha.nguyen@tomosia.com>
2025-09-25 08:59:33 +0700

    refactor

--- socket/index.js
+++ socket/index.js
@@ -12,24 +12,24 @@ const wss = new WebSocketServer({ noServer: true })
 const port = process.env.PORT || 1234
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('okay')
 })
 
 wss.on('connection', setupWSConnection)
 
 server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here..
-  // See https://github.com/websockets/ws#client-authentication
-  /**
-   * @param {any} ws
-   */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
-  }
-  wss.handleUpgrade(request, socket, head, handleAuth)
+    //   // You may check auth of request here..
+    //   // See https://github.com/websockets/ws#client-authentication
+    //   /**
+    //    * @param {any} ws
+    //    */
+    const handleAuth = ws => {
+        wss.emit('connection', ws, request)
+    }
+    wss.handleUpgrade(request, socket, head, handleAuth)
 })
 
 server.listen(port, () => {
-  console.log(`running on port ${port}`)
+    console.log(`running on port ${port}`)
 })
