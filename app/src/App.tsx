6d9ddec198ac8559e45d40cc4412230585435f70
nha <nha.nguyen@tomosia.com>
2025-10-09 16:58:23 +0700

    Fix bug missed last message

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ import Modal from './components/Modal'
 import useImage from 'use-image'
 
 export default function App() {
-  const room = useMemo(() => getQueryParam('room', 'public-2'), [])
+  const room = useMemo(() => getQueryParam('room', 'public222'), [])
   const [fishImage] = useImage('/fish.png')
 
   const [userName, setUserName] = useState(
